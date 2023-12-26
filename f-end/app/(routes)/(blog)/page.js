cc946f744beeed79005493e182135f7e1c9e8df7
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2023-12-26 23:12:08 +0530

    Connected back-end api with Next

--- f-end/app/(routes)/(blog)/page.js
+++ f-end/app/(routes)/(blog)/page.js
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+const LOCAL_DOMAIN = 'http://localhost:5000'
 export default function Home() {
-  return (
-    <h1>THIS IS HOME PAGE</h1>
-  )
+  useEffect(() => {
+    fetch(`${LOCAL_DOMAIN}/api/home`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message);
+      });
+  }, []);
+  return <h1>THIS IS HOME PAGE</h1>;
 }
