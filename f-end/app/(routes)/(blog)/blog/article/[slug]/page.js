dfab084ab95205c451377af6950185aeb83ea287
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2024-01-19 23:42:11 +0530

    Did some changes in Contexts Component

--- f-end/app/(routes)/(blog)/blog/article/[slug]/page.js
+++ f-end/app/(routes)/(blog)/blog/article/[slug]/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({params}) {
   return (
     <>
       {/* HEADER COMPONENT > LOGO+NAVBAR_C+SOCIAL_C */}
       {/* SINGLE BLOG POST_C */}
-      <h1>THIS IS SINGLE ARTICLE PAGE </h1>
+      <h1 className="text-white">THIS IS SINGLE ARTICLE PAGE </h1>
     </>
   );
 }
--- f-end/app/_Components/_Blog/Contexts.jsx
+++ f-end/app/_Components/_Blog/Contexts.jsx
@@ -8,7 +8,11 @@ export const ActiveNavContext = createContext();
 
 const Contexts = ({ children }) => { // works as parent component having state which is shared by each of its children component
     const activeMenuLink = usePathname()
-    const activeMenu = navLinks.find((obj) => obj.link === activeMenuLink).id
+    // const activeMenu = (navLinks.find((obj) => obj.link === activeMenuLink).id)
+    const foundObject = navLinks.find((obj) => obj.link === activeMenuLink); // check whether menu is there in database or not
+    const activeMenu = foundObject ? foundObject.id : 'blog'; //if true, store that menu id, else store another value
+
+
     const [active, setActive] = useState(activeMenu) // active navbar menu
     useEffect(() => {
         if (activeMenuLink === '/') {
@@ -18,7 +22,7 @@ const Contexts = ({ children }) => { // works as parent component having state w
             document.body.style.backgroundColor = '#ECECEC'
 
         }
-    },[])
+    }, [])
 
     return (
         <ActiveNavContext.Provider value={{ active, setActive }}>
