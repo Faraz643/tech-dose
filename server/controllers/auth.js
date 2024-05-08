1ac0a5493c7545da63ba28e61743f8c1688d493d
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2024-05-08 19:45:27 +0530

    Implement sample admin sign-up form-logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,49 @@
-export const adminSignup = (req, res) => {
-  res.json({ status: "Admin Sign Up" });
+import { connection } from "../db.config.js";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = "538c3d37acf0995cfbd51276c0f1053d";
+
+export const adminSignup = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ message: "No form data received" });
+  }
+  const { enrollmentId, password } = req.body;
+  const addNewUserQuery = `INSERT INTO users (enroll_id, password) VALUES (?, ?)`;
+  connection
+    .query(addNewUserQuery, [enrollmentId, password])
+    .then(() => res.status(201).json({ message: "New User Added" }))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Error Occured" });
+    });
 };
 
 // @middleware -> validate user data and user role, access token
-export const adminSignIn = (req, res) => {
-  res.json({ status: "Admin Sign In" });
+export const adminSignIn = async (req, res) => {
+  const { enrollmentId, password } = req.body;
+  if (!enrollmentId || !password) {
+    return res
+      .status(400)
+      .json({ message: "Missing enrollment ID or password" });
+  }
+  const getUserQuery = `SELECT * FROM users WHERE enroll_id=?`;
+  await connection
+    .query(getUserQuery, [enrollmentId])
+    .then((result) => {
+      if (!result[0].length) {
+        res.status(401).json({ message: "User Not Found" });
+      }
+      const userPass = result[0][0].password;
+      if (enrollmentId && password === userPass) {
+        const token = jwt.sign({ enrollmentId }, SECRET_KEY, {
+          expiresIn: "30m",
+        });
+        res.json({ GeneatedToken: token });
+      } else {
+        res.status(401).json({ message: "User exists but wrong password" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,6 @@ import { adminSignIn, adminSignup } from "../controllers/auth.js";
 const router = express.Router();
 
 router.post("/sign-up", adminSignup); // input regex validation middleware
-router.get("/sign-in", adminSignIn); // validation middleware
+router.post("/sign-in", adminSignIn); // validation middleware
 
 export default router;
