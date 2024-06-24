0105db1e11ec6163caaa405cf024f4197dc956bf
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2024-06-25 00:15:58 +0530

    Commit code for review

--- server/index.js
+++ server/index.js
@@ -43,6 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.raw({ limit: "1mb" }));
 app.use(express.static("images"));
 app.use(cookieParser());
+// allow origin
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
