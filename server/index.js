95cdb7990eca81a42dbf52d19679e222a6bfad71
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2024-07-21 00:13:00 +0530

    allow origin

--- server/index.js
+++ server/index.js
@@ -41,20 +41,22 @@ const port = process.env.PORT || 3000;
 //   credentials: true
 // };
 
-
 // Allow requests from your frontend origin
-const allowedOrigin = 'https://tech-dose.vercel.app';
-
-app.use(cors({
-    origin: process.env.FRONT_END_ORIGIN,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // If you need to include cookies in the requests
-}));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://tech-dose.vercel.app",
+];
+app.use(
+  cors({
+    origin:allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // If you need to include cookies in the requests
+  })
+);
 
 // Enable pre-flight handling
-app.options('*', cors());
-
+app.options("*", cors());
 
 // app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
