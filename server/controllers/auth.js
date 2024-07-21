796b8957dc4b95d69e448b3b8a5843736b1f5ea0
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2024-07-21 22:17:30 +0530

    Add expiration time for cookie

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -124,6 +124,7 @@ export const adminSignIn = async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
+        maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
       });
       return res.json({ message: "User Session Created" });
     } else {
