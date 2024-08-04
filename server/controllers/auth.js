eb1d1b52800038638ab1403962f68fbcf1402938
Faraz Rasool <farazrasool643@gmail.com>
2024-08-04 23:49:22 +0530

    modify authjs(14)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -125,7 +125,7 @@ export const adminSignIn = async (req, res) => {
         maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
         sameSite: "none",
         secure: true,
-        domain: "https://tech-dose-view.onrender.com",
+        domain: "tech-dose-view.onrender.com",
         path: "/",
       });
       return res.json({ message: "User Session Created" });
