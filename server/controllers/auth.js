21541c6514249aedff72a8ec24e6f409bc24fd81
Faraz Rasool <farazrasool643@gmail.com>
2024-08-04 15:21:52 +0530

    change cookie config

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -125,6 +125,8 @@ export const adminSignIn = async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
+        domain:'.tech-dose.vercel.app',
+        path:'/',
         maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
       });
       return res.json({ message: "User Session Created" });
